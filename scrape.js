a2bd563db6e89a18b236d1014b292482d7195e68
yuthmikagamage <vanujyuthmika@gmail.com>
2025-10-22 20:09:07 +0530

    Get animal details

--- scrape.js
+++ scrape.js
@@ -1,10 +1,42 @@
 import jsdom from "jsdom";
+import fs, { link } from "fs";
 
 const textContext = await fetch("https://a-z-animals.com/animals/").then(
   (response) => response.text()
 );
 const htmlContent = new jsdom.JSDOM(textContext);
 const htmlDocument = htmlContent.window.document;
-const item = htmlDocument.querySelector(".list-item.col-md-4.col-sm-6");
-const itemA = item.querySelector("a");
-console.log(itemA.textContent);
+const list = htmlDocument.querySelectorAll(".list-item.col-md-4.col-sm-6");
+
+const finalOutput = [];
+
+for (let i = 0; i < 3; i++) {
+  const item = list[i];
+  const aElement = item.querySelector("a");
+  const itemUrl = aElement.href;
+  const itemTextContent = await fetch(itemUrl).then((response) =>
+    response.text()
+  );
+  const itemHtmlContent = new jsdom.JSDOM(itemTextContent);
+  const itemHtmlDocument = itemHtmlContent.window.document;
+
+  const detailsList = itemHtmlDocument.querySelectorAll(
+    ".col-sm-3.text-md-right"
+  );
+
+  const detailsOutput = [];
+
+  for (let i = 0; i < detailsList.length; i++) {
+    const detailsAElement = detailsList[i].querySelector("a");
+    const detailsTopic = detailsAElement.textContent;
+    const detailsDescriptionItem = detailsList[i].nextElementSibling;
+    const detailsDescription = detailsDescriptionItem.innerHTML;
+    detailsOutput.push(`${detailsTopic} : ${detailsDescription}`);
+  }
+  finalOutput.push({
+    Name: aElement.textContent,
+    Link: aElement.href,
+    Details: detailsOutput,
+  });
+}
+fs.writeFileSync("outputs/animals.json", JSON.stringify(finalOutput, null, 2));
